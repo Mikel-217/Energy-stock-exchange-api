4710051f936bd94c1c652a42c191253b251a9d19
Mikel-217 <Mikel.Kunze887@web.de>
2026-03-20 13:15:15 +0100

    Added database startup | Added dockerignore | Added table json

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"mikel-kunze.com/energy-stock-exchange-api/logging"
+	"mikel-kunze.com/energy-stock-exchange-api/startup"
+)
+
 func main() {
 
+	// Create the tables -> if it fails we cannot continue
+	if !startup.CreateDatabaseTables() {
+		fmt.Println("Check the logs and your database connection! \n Program cannot start :(")
+		os.Exit(1)
+	}
+
+	mux := http.NewServeMux()
+
+	// starts the server and checks for a error
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		logging.Log(logging.Error, err.Error())
+		os.Exit(1)
+	}
 }
