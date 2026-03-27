0c00b96d2170a558e1defca289812637d26b3396
Mikel-217 <mikel.kunze@protonmail.com>
2026-03-27 16:32:16 +0100

    Added insert and update funcs to database

--- database/database_structs/energy_price.go
+++ database/database_structs/energy_price.go
@@ -1,6 +1,11 @@
 package databasestructs
 
-import "time"
+import (
+	"time"
+
+	"mikel-kunze.com/energy-stock-exchange-api/database"
+	"mikel-kunze.com/energy-stock-exchange-api/logging"
+)
 
 type EnergyPriceStruct struct {
 	EnergyPriceId  uint // Primary key
@@ -9,8 +14,30 @@ type EnergyPriceStruct struct {
 	BestTimeToSell uint // foreign key
 }
 
-func (e *EnergyPriceStruct) InsertIntoDatabase() {
-	// TODO
+// Inserts the given EnergyPriceStruct into the database
+func (e *EnergyPriceStruct) InsertIntoDatabase() bool {
+
+	query := "INSERT INTO EnergyPrice() VALUES(DEFAULT, ?, ?, ?);"
+	queryArgs := []any{e.CurrentDate, e.BestTimeToBuy, e.BestTimeToSell}
+
+	if result := database.ExecuteSQL(query, queryArgs); result.ErrorMsg != nil {
+		logging.Log(logging.Error, result.ErrorMsg.Error())
+		return false
+	}
+	return true
+}
+
+func (e *EnergyPriceStruct) UpdateBestTimes(bestTimeToById, bestTimeToSellId uint) bool {
+
+	query := "UPDATE EnergyPrice SET BestTimeToBuy = ?, BestTimeToSell = ? WHERE EnergyPriceId = ?;"
+	queryArgs := []any{bestTimeToById, bestTimeToSellId, e.EnergyPriceId}
+
+	if result := database.ExecuteSQL(query, queryArgs); result.ErrorMsg != nil {
+		logging.Log(logging.Error, result.ErrorMsg.Error())
+		return false
+	}
+
+	return true
 }
 
 type DateAndPriceStruct struct {
@@ -20,6 +47,17 @@ type DateAndPriceStruct struct {
 	EnergyPriceId uint
 }
 
-func (d *DateAndPriceStruct) InsertIntoDatabase() {
-	// TODO
+// Inserts the given DataAndPriceStruct
+func (d *DateAndPriceStruct) InsertIntoDatabase() *database.Result {
+
+	query := "INSERT INTO DateAndPrice() VALUES(DEFAULT, ?, ?, ?);"
+	queryArgs := []any{d.Date, d.Price, d.EnergyPriceId}
+
+	result := database.ExecuteSQL(query, queryArgs)
+
+	if result.ErrorMsg != nil {
+		logging.Log(logging.Error, result.ErrorMsg.Error())
+	}
+
+	return result
 }
